70552896f2d16257903708dce7ea5bc8bd018e5e
inquisitivefiza <khanfiza3dec004@gmail.com>
2026-06-27 00:13:07 +0530

    feat:added exception file

---
+++ src/main/java/com/example/stayfinder/backend/exception/AccessDeniedException.java
@@ -0,0 +1,8 @@
+package com.example.stayfinder.backend.exception;
+
+
+public class AccessDeniedException extends RuntimeException {
+    public AccessDeniedException(String message) {
+        super(message);
+    }
+}
